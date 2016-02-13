255c39778f09e53a3b83dd6c9dbeaa961dff5a8a
Anshu <bajpai.anshumaan@gmail.com>
2016-02-13 09:55:29 -0500

    Ch6

--- workspace/tango_with_django_project/rango/models.py
+++ workspace/tango_with_django_project/rango/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Creating the Category model
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
+
+
+# Creating the Page model
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title

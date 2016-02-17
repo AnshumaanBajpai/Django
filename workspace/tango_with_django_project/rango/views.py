3a0a11ad85b985373a6e488a81ad3568b635a0b7
Anshu <bajpai.anshumaan@gmail.com>
2016-02-17 00:49:30 -0500

    Ch10

--- workspace/tango_with_django_project/rango/views.py
+++ workspace/tango_with_django_project/rango/views.py
@@ -181,7 +181,7 @@ def user_login(request):
         
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 from django.contrib.auth import logout
 
